04c256d076f3ccec7be3c009276b2b9c395a6f22
Ting Deng <maidenbelize@gmail.com>
2016-06-12 22:00:00 -0700

    set up keys

---
+++ server/keys.js
@@ -0,0 +1,15 @@
+module.exports = {
+  NPR_APIKEY: "MDIxOTA1NTg5MDE0NTEzMjgxNDhiMmVhMg000",
+  SENDGRID_APIKEY: "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",
+  MAILGUN_APIKEY: "key-1ad868c49592d4a8c9ff34a19fcfb516",
+}
+
+/* API DOCUMENTATIONS
+
+SENDGRID EMAIL SAMPLES
+https://sendgrid.com/docs/Classroom/Send/v3_Mail_Send/curl_examples.html
+
+SENDGRID FULL DOCS
+https://github.com/sendgrid/sendgrid-nodejs
+
+*/
